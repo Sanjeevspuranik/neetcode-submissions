d890a7ebc6c6a998592fa16534c9c14abdc9bcfb
Sanjeevspuranik <139968855+Sanjeevspuranik@users.noreply.github.com>
2026-08-22 01:32:40 +0530

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.py
@@ -0,0 +1,26 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+        
+        char_s1 = [0] * 26
+        char_s2 = [0] * 26
+
+        for i in range(len(s1)):
+            char_s1[ord(s1[i]) - ord('a')] += 1
+            char_s2[ord(s2[i]) - ord('a')] += 1
+        
+        if char_s1 == char_s2:
+            return True
+        
+        left = 0
+        for right in range(len(s1), len(s2)):
+            char_s2[ord(s2[right]) - ord('a')] += 1
+            char_s2[ord(s2[left]) - ord('a')] -= 1
+
+            left += 1
+            if char_s1 == char_s2:
+                return True
+        
+        return False
+        
